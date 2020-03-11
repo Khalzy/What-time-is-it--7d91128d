747bae1729e461bde080f2f596ad192f80c1fd0d
Candace <2100978@talnet.nl>
2020-03-11 11:57:27 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const pi = 3.14;
 let numWaarde;
 let result;
-var d = Date();
-document.getElementById("date").innerText = d;
+function time() {
+  var d = Date();
+  document.getElementById("date").innerText = d;
+}
+time();
+setInterval(time, 1);
 function calculate() {
   numWaarde = document.getElementById("num").value;
   result = pi * numWaarde;
